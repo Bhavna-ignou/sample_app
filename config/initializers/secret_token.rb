57178b42fbe5d1048b9cdb1e67df6a2fdbe172bd
rvm --list <rvm list>
2013-03-29 12:14:37 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '39792c505981c3a384df4854f1db8ef4da55ad502d8074e2c19f62376a406a913dbea14dec819bf090b02d12d52483e4a812d7f6832ed0c3e74e54c17414e984'
